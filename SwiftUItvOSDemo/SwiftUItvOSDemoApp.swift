1cff991bb3653aea8d5453712955e91fd3f0e6e2
Yogendra Singh <000yogi1990@gmail.com>
2023-09-24 12:17:03 +0530

    Initial Commit

---
+++ SwiftUItvOSDemo/SwiftUItvOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItvOSDemoApp.swift
+//  SwiftUItvOSDemo
+//
+//  Created by Yogendra Singh on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItvOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
